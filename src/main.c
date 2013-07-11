aab1594ff79609f14cfad0502ac867602f1079af
targuan <>
2013-07-12 00:11:44 +0200

    add send

--- src/main.c
+++ src/main.c
@@ -47,12 +47,38 @@ void phex(unsigned char *p, int len) {
 
 }
 
+void inverse_ip(struct iphdr *i) {
+    uint32_t t;
+    
+    t = i->daddr;
+    i->daddr = i->saddr;
+    i->saddr = t;
+}
+
+void inverse_eth(struct ether_header *e) {
+    struct ether_header t;
+
+    memcpy(&t, e, sizeof (struct ether_header));
+
+    memcpy(e->ether_shost, t.ether_dhost, sizeof (t.ether_dhost));
+    memcpy(e->ether_dhost, t.ether_shost, sizeof (t.ether_dhost));
+}
+
+void inverse_udp(struct udphdr *u) {
+    int src;
+
+    src = u->source;
+    u->source = u->dest;
+    u->dest = src;
+}
+
 int main(int argc, char** argv) {
     int rawsock;
     struct sockaddr_ll sll;
     struct ifreq ifr;
-    int len, sll_size;
+    int len, sll_size, pid;
     void* packet_buffer;
+    char *dns;
 
     int family;
 
@@ -61,7 +87,7 @@ int main(int argc, char** argv) {
     struct ip6_hdr *ip6;
     struct udphdr *udp;
 
-    packet_buffer = calloc(2048,1);
+    packet_buffer = calloc(2048, 1);
 
 
     if ((rawsock = socket(PF_PACKET, SOCK_RAW, IPPROTO_RAW)) == -1) {
@@ -72,7 +98,7 @@ int main(int argc, char** argv) {
     bzero(&sll, sizeof (sll));
     bzero(&ifr, sizeof (ifr));
 
-    strncpy((char *) ifr.ifr_name, "eth2", IFNAMSIZ);
+    strncpy((char *) ifr.ifr_name, "eth1", IFNAMSIZ);
     if ((ioctl(rawsock, SIOCGIFINDEX, &ifr)) == -1) {
         printf("Error getting Interface index !\n");
         exit(-1);
@@ -83,6 +109,7 @@ int main(int argc, char** argv) {
     sll.sll_family = AF_PACKET;
     sll.sll_ifindex = ifr.ifr_ifindex;
     sll.sll_protocol = htons(ETHERTYPE_IP);
+    sll.sll_halen = ETH_ALEN;
 
 
     if ((bind(rawsock, (struct sockaddr *) &sll, sizeof (sll))) == -1) {
@@ -90,6 +117,8 @@ int main(int argc, char** argv) {
         exit(-1);
     }
 
+    fork();
+    
     while (1) {
         if ((len = recvfrom(rawsock, packet_buffer, 2048, 0, NULL, NULL)) == -1) {
             perror("Recv from returned -1: ");
@@ -109,12 +138,25 @@ int main(int argc, char** argv) {
                 udp = (struct udphdr *) (((void *) ip6) + IP6_HDR_LEN);
             }
 
-           
+            
 
 
             if (ntohs(udp->dest) == 53) {
+                dns = (((void *) udp) + sizeof(struct udphdr));
                 printf("Received %d\n", ntohs(udp->dest));
                 phex(packet_buffer, len);
+                inverse_eth(eth);
+                inverse_ip(ip);
+                inverse_udp(udp);
+                udp->check = 0;
+                dns[2] = dns[2]|0x80;
+                dns[3] = 0x80;
+                phex(packet_buffer, len);
+                
+                if (sendto(rawsock, packet_buffer, len, 0, (struct sockaddr*) &sll, sizeof (struct sockaddr_ll)) < 0)
+                        printf("Send failed\n");
+                else
+                    printf("Send OK\n");
                 break;
             }
         }
